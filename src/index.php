d0b47a35f6b0df8679324349e61c1fbdbb432ad0
brandoncamenisch <brandoncamenisch@gmail.com>
2019-05-15 15:33:31 -0700

    💧 drip - master: adds echo line

--- src/index.php
+++ src/index.php
@@ -1,6 +1,7 @@
 <?php
 
 echo "Hello, World from Kubernetes! <br>";
+echo "WUT! <br>";
 echo "Hello Jenkins X! <br>";
 echo '<img border="0" src="https://jenkins-x.io/images/logo.png" width="300" height="300" />';
 
