f285fc6ac14c7984c6824d475ddcc8f2e3653d49
brandoncamenisch <brandoncamenisch@gmail.com>
2019-05-15 15:44:12 -0700

    💧 drip - feature/tests-pipe: adds echo line

--- src/index.php
+++ src/index.php
@@ -2,6 +2,7 @@
 
 echo "Hello, World from Kubernetes! <br>";
 echo "WUT! <br>";
+echo "WUT! <br>";
 echo "Hello Jenkins X! <br>";
 echo '<img border="0" src="https://jenkins-x.io/images/logo.png" width="300" height="300" />';
 
